a4c4b2c01b35977c581688e0816dbd2b7a9e12f1
ivan.cheremisin <icheremisin@gmail.com>
2025-05-15 17:51:36 -0300

    Commit all outstanding changes

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -4,11 +4,10 @@ export const generateMetadata = (): Metadata => {
     return {
         title: 'Home',
         description:
-            'Portfolio of Ivan Cheremisin, Product Designer specializing in AI Tools, B2B, and SaaS.',
+            'Ivan Cheremisin – Product Designer specializing in AI Tools, B2B, and SaaS.',
         openGraph: {
             title: 'Ivan Cheremisin — Product Designer',
-            description:
-                'Portfolio of Ivan Cheremisin, Product Designer specializing in AI Tools, B2B, and SaaS.',
+            description: 'Specializing in AI Tools, B2B, and SaaS.',
             images: [
                 {
                     url: '/og-image.jpg',
@@ -21,8 +20,7 @@ export const generateMetadata = (): Metadata => {
         twitter: {
             card: 'summary_large_image',
             title: 'Ivan Cheremisin — Product Designer',
-            description:
-                'Portfolio of Ivan Cheremisin, Product Designer specializing in AI Tools, B2B, and SaaS.',
+            description: 'Specializing in AI Tools, B2B, and SaaS.',
             images: ['/og-image.jpg'],
         },
     }
